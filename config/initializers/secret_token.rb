354064555be2cc57b33f08e03c388288e39b080c
mohanraj-nagasamy <mohanraj.nagasamy@octanner.com>
2013-06-24 15:40:17 -0600

    helloworld

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails40Helloworld::Application.config.secret_key_base = '2b8d6fdbf49fed9e39955ad80da9cba02ebf8338ab32160ae299dfb6a7de91281d4fa4d70d0fbcdf7fb0083a236ff0164b33851f76d4f7467efa6c2f1e219865'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails40Helloworld::Application.config.session_store :encrypted_cookie_store, key: '_rails4_0-helloworld_session'
